606257d1f21103f2571d5af61fe8d1ff025827d9
Israel192619 <israel.zc.09@gmail.com>
2022-11-23 11:05:51 -0400

    CIRCLE_IMAGE_VIEW Y SIMPLE_VIEW_BEHAVIOR 14

--- app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.example.infogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.infogram.R;
+import com.example.infogram.adapter.CardViewAdapter;
+import com.example.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,48 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolBar("",false, view);
+
+        //recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_profile);
+
+        //layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("https://www.laregion.bo/wp-content/uploads/2019/12/caraaa.jpg", "Carlos Valdivia", "2 dias", "1 me gusta"));
+        images.add(new Image("https://www.boliviaentusmanos.com/turismo/imagenes/casa-de-la-moneda-1.jpg", "Luis Perez", "3 dias", "5 me gusta"));
+        images.add(new Image("https://lh5.googleusercontent.com/-CINb2hHdBHc/VMfJjASBQDI/AAAAAAAAC0I/23gSET6uSaw/s640/blogger-image-979580039.jpg", "Fernando Torricos", "6 dias", "4 me gusta"));
+        images.add(new Image("https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6f/46/35.jpg", "Maria Flores", "5 dias", "6 me gusta"));
+        images.add(new Image("https://www.viajesylugares.com/imagenes/copacabana505101_960_720.jpg", "Rolando Morales", "3 dias", "5 me gusta"));
+        images.add(new Image("https://www.noticiasfides.com/images/news/2022/09/c-image2022-09-15163039220-417754-A1C5.png", "William Jaimes", "4 dias", "4 me gusta"));
+        images.add(new Image("https://www.magazinemanagement.gm-bolivia.com/wp-content/uploads/2021/11/6030819859692969.jpg", "Roberto Paniagua", "2 dias", "3 me gusta"));
+        images.add(new Image("https://samaipata.com/wp-content/uploads/2017/03/1a.jpg", "Lorena Herrera", "2 dias", "8 me gusta"));
+        images.add(new Image("https://www.boliviaentusmanos.com/turismo/imagenes/cristo-de-la-concordia-1.jpg", "Laura Caceres", "6 dias", "3 me gusta"));
+        images.add(new Image("https://www.caxiatours.com/img/packages/normal_d8acf0cbd512140f782bc72f46631cbce14b8cce.jpg", "Marcelo Rojas", "9 dias", "1 me gusta"));
+
+        return images;
     }
 }
