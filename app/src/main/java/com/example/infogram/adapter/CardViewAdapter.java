efb8287b099a77bf85d5cbcad4ef8ab026b3ad15
Israel192619 <israel.zc.09@gmail.com>
2022-11-23 09:40:16 -0400

    COLLAPSING_TOOLBAR_LAYOUT 13

--- app/src/main/java/com/example/infogram/adapter/CardViewAdapter.java
+++ app/src/main/java/com/example/infogram/adapter/CardViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.infogram.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.infogram.R;
 import com.example.infogram.model.Image;
+import com.example.infogram.view.ImageDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -44,6 +46,15 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.CardVi
         holder.usernameCardview.setText(image.getUsername());
         holder.cantidadDiasCardView.setText(image.getCantidadDias());
         holder.cantidadMegustaCardView.setText(image.getCantidadMeGusta());
+
+        //onclickListener
+        holder.imageCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, ImageDetailActivity.class);
+                activity.startActivity(intent);
+            }
+        });
     }
 
     @Override
